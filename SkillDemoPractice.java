a9425dafb306d22fdfd39256cea15e0b71ff951d
harshi-cse <Harsa.throwaway@gmail.com>
2022-02-02 20:45:58 -0800

    Update SkillDemoPractice.java

--- SkillDemoPractice.java
+++ SkillDemoPractice.java
@@ -1,7 +1,8 @@
 public class SkillDemoPractice {
     public static boolean isFactorOf(int x, int y) {
-	if ( y != 0 && x%y == 0) {
-		return true;
+        if ( y != 0 && x%y == 0) {
+            return true;
+        }
+        return false;
     }
-    return false;
 }
